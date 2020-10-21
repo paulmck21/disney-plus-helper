581d2a4fee9b7dc9a85fbe6f89981346e3ea27b8
Paul Mckenna <paul@zeroheight.com>
2020-10-21 10:07:21 +0100

    remove unneeded activetab request

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,7 @@ document.addEventListener('keydown', (e) => {
     if (e.code !== 'Space') {
         return;
     }
+    e.preventDefault();
     const video = document.getElementsByTagName('video')[0];
     if (!video) {
         return;
